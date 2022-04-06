29db971862a4c8adaab68e8582958c0ba4ea00f1
theanh2906 <theanh2906@outlook.com>
2022-04-06 22:27:23 +0700

    allow cors

--- src/main/java/com/backend/api/ApiBackendApplication.java
+++ src/main/java/com/backend/api/ApiBackendApplication.java
@@ -2,10 +2,23 @@ package com.backend.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ApiBackendApplication {
-
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:4200", "https://devontop.com")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
     public static void main(String[] args) {
         SpringApplication.run(ApiBackendApplication.class, args);
     }
